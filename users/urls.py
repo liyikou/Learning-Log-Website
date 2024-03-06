a044ccbeccaae4edea18e6cb1799b3d514efe0d1
liyikou <hanpengxinn@qq.com>
2024-03-06 15:59:02 +0800

    feat: Add logout function.

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
 
 app_name = 'users'
--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 class Login(LoginView):
     template_name = 'users/login.html'
+    
+def logout_view(request):
+    logout(request)  # 登出用户，清除用户的session
+    return HttpResponseRedirect(redirect_to=reverse('learning_logs:index'))
 
